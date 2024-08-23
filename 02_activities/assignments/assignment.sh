8cf3e5d001b3e2b69eb8ccced523822fab7575b5
IrynaHorodetska88 <iryna.v.horodetska@gmail.com>
2024-08-23 18:50:55 -0400

    Update assignment.sh syntax

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -12,7 +12,6 @@ set -x
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
   ls 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-  cd dir2
   touch file1.txt file2.txt file3.txt file4.txt file5.txt
 # 4. Append the words "Hello world" to dir2/file3
  echo "Hello world" >> file3.txt
