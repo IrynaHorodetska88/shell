0c697051a07496a9b293d5c4332a87a599a1f56c
IrynaHorodetska88 <iryna.v.horodetska@gmail.com>
2024-09-04 19:55:09 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -11,10 +11,10 @@ set -x
   mkdir dir1 dir2 dir3 dir4 dir5
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
   ls 
-# 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
+# 3. Create 5 text files in dir2 named file1.txt, file2.txt, file3.txt, file4.txt, and file5.txt
   cd dir2
   touch file1.txt file2.txt file3.txt file4.txt file5.txt
-# 4. Append the words "Hello world" to dir2/file3
+# 4. Append the words "Hello world" to dir2/file3.txt
  echo "Hello world" >> file3.txt
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
   cat file3.txt
