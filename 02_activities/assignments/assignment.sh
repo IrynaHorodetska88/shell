b38eb7eab6d4d7c77f9a42447930a232825f1fa6
IrynaHorodetska88 <iryna.v.horodetska@gmail.com>
2024-09-04 19:48:43 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -17,9 +17,9 @@ set -x
 # 4. Append the words "Hello world" to dir2/file3
  echo "Hello world" >> file3.txt
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
-  cat file3
+  cat file3.txt
 # 6. Delete file4
-  rm file4
+  rm file4.txt
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
   cd ..
   rm -r dir4 dir5
